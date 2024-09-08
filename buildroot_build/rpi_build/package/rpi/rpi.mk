5e96e0fc6e9db99727192bc88a76b5a8bb6bc74e
gpcm00 <gianpcm@gmail.com>
2024-09-07 23:52:28 -0700

    wifi working

--- buildroot_build/rpi_build/package/rpi/rpi.mk
+++ buildroot_build/rpi_build/package/rpi/rpi.mk
@@ -4,23 +4,23 @@
 #
 ##############################################################
 
-RPI_VERSION = 'dd93969'
+RPI_VERSION = '2b5f0dc'
 RPI_SITE = 'git@github.com:gpcm00/power-modulator.git'
 RPI_SITE_METHOD = git
 RPI_GIT_SUBMODULES = YES
 RPI_LICENSE = 'Dual BSD/GPL'
 RPI_LICENSE_FILES = COPYING
 RPI_MODULE_SUBDIRS = hello_module
-RPI_MODULE_SUBDIRS += devices/powermodul
+RPI_MODULE_SUBDIRS += devices/powercontrol
 
 define RPI_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/devices/powermodul dt
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/devices/powercontrol dt
 endef
 
 define RPI_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/test $(TARGET_DIR)/test
-	$(INSTALL) -m 0755 $(@D)/devices/powermodul/powmodul.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/pownmodul.dtbo
+	$(INSTALL) -m 0755 $(@D)/devices/powercontrol/powctl.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/powctl.dtbo
 endef
 
 $(eval $(kernel-module))
